d972c2cbfe8aec3333a82d91d5774aa6ead6fe3b
张新荣 <2914424377@qq.com>
2024-01-20 20:02:42 +0800

    添加 公共模块

---
+++ booboilOJ-backend-common/src/main/java/com/booboil/booboilojbackendcommon/BooboilOjBackendJudgeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.booboil.booboilojbackendcommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooboilOjBackendJudgeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooboilOjBackendJudgeServiceApplication.class, args);
+    }
+
+}
---
+++ booboilOJ-backend-common/src/main/java/com/booboil/booboilojbackendcommon/common/DeleteRequest.java
@@ -0,0 +1,22 @@
+package com.booboil.booboilojbackendcommon.common;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 删除请求
+ *
+ * @author <a href="https://github.com/booboil">程序员鱼皮</a>
+ * 
+ */
+@Data
+public class DeleteRequest implements Serializable {
+
+    /**
+     * id
+     */
+    private Long id;
+
+    private static final long serialVersionUID = 1L;
+}
