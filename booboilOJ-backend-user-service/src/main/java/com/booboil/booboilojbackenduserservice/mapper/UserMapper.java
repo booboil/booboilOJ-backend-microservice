f8d8797e736a451cafc8ac25e44cf7b657ec1929
张新荣 <2914424377@qq.com>
2024-01-21 22:22:35 +0800

    添加用户模块 用户服务项目设置

---
+++ booboilOJ-backend-user-service/src/main/java/com/booboil/booboilojbackenduserservice/mapper/UserMapper.java
@@ -0,0 +1,19 @@
+package com.booboil.booboilojbackenduserservice.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.booboil.booboilojbackendmodel.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ * @author <a href="https://github.com/booboil">程序员booboil</a>
+ *
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
