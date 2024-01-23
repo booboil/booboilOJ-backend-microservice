a8d2c33d2645d31b5b73a2819a10aca3c753dac0
张新荣 <2914424377@qq.com>
2024-01-23 14:15:40 +0800

    gateway网关过滤

--- booboilOJ-backend-gateway/src/main/java/com/booboil/booboilojbackendgateway/BooboilOjBackendGatewayApplication.java
+++ booboilOJ-backend-gateway/src/main/java/com/booboil/booboilojbackendgateway/BooboilOjBackendGatewayApplication.java
@@ -2,10 +2,14 @@ package com.booboil.booboilojbackendgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
-@SpringBootApplication
+// 不需要加载数据库配置
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 @EnableDiscoveryClient
+@EnableSwagger2WebMvc
 public class BooboilOjBackendGatewayApplication {
 
     public static void main(String[] args) {
